d613d0cb0f316713b3a1177e5834e12777dfd251
Jack Keane <john.f.keane.22@dartmouth.edu>
2021-06-01 21:47:43 -0600

    life on mars page complete

--- src/components/LifeOnMars.js
+++ src/components/LifeOnMars.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { marsWeather } from '../constants/urls';
-import landscapeImg from '../img/marslandscape.png';
 
 // Cameras used each day are not consistent, so this ensures consistency in quality images
 // const cameraPriority = {
@@ -47,11 +46,36 @@ const LifeOnMars = () => {
 
   return (
     <section className="life-on-mars">
-      <h1 id="lom">Life on Mars</h1>
-      <img src={landscapeImg} alt="Mars Landscape" width="60%" />
-      <p className="temperature high">High: {weather.max_temp}°C</p>
-      <p className="temperature low">Low: {weather.min_temp}°C</p>
-      {weather.atmo_opacity && <p>The weather is {String(weather.atmo_opacity).toLowerCase()}</p>}
+      <div className="left">
+        <h1 id="lom">Life on Mars</h1>
+        <div className="landscape">
+          <div className="weather-pane">
+            <h2>Latest Weather:</h2>
+            {weather.atmo_opacity && <p>Conditions: {weather.atmo_opacity}</p>}
+            {weather.max_temp && <p>High: {weather.max_temp}°C</p>}
+            {weather.min_temp && <p>Low: {weather.min_temp}°C</p>}
+            {weather.local_uv_irradiance_index && <p>UV Radiation: {weather.local_uv_irradiance_index}</p>}
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <div className="facts">
+          <h3>Soak in the rays</h3>
+          <p>Mars&apos; minimal magnetosphere and atmosphere provides optimal exposure to the cosmos.</p>
+        </div>
+        <div className="facts">
+          <h3>Storms like none other</h3>
+          <p>If you are lucky, you may get the chance to experience a global dust storm, which can last for weeks at a time.</p>
+        </div>
+        <div className="facts">
+          <h3>Jump for the heavens</h3>
+          <p>The gravity on Mars has only 38% of Earth&apos;s gravity, which means you can jump 2.64x higher. Have fun with that!</p>
+        </div>
+        <div className="facts">
+          <h3>Get your breath taken away</h3>
+          <p>Literally. There is no oxygen in Mars&apos; atmosphere, its mostly carbon dioxide. Besides, the atmospheric pressure is 1% that of Earth&apos;s</p>
+        </div>
+      </div>
     </section>
   );
 };
